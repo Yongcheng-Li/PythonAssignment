6dab450530499c9ff290ddce7218e39e29156bc9
yli46UNB3035 <yli46@unb.ca>
2021-09-19 11:30:53 +0800

    q1

--- StudentCode/Q1/Dog.py
+++ StudentCode/Q1/Dog.py
@@ -1,14 +1,4 @@
 
-class Dog:
-    def __init__(self, name, weight, days):
-        self.name = name
-        self.weight = weight
-        self.days = days
-
-    
-    def isSenior(self):
-        return False
-          
 
         
 
